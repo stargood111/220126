8545b3adb96d7d5b50015724a6312d210f1cee36
최인석 <stargood37@naver.com>
2022-01-26 16:20:03 +0900

    20220126

--- 모듈.py
+++ 모듈.py
@@ -1,13 +1,25 @@
-from moduletest import square
-from math import pow
+#
+# from moduletest import square
+# from math import pow
+# import pandas
+# import numpy
+#
+# import moduletest as md
+#
+# print(square(10,20))
+# pow(10,3)
+#
+#
+# md.square(3.4)
+#
+a = '대한민국'
+try:
+    b=10/a
+except ZeroDivisionError:
+    print('0나누기 에러')
+except TypeError:
+    print('문자로 나누기 에러')
+except:
+    print('모든 에러:',e.args[0])
+
 import pandas
-import numpy
-
-import moduletest as md
-
-print(square(10,20))
-pow(10,3)
-
-
-md.square(3.4)
-
